44cbca31c34a9e7efe18c9d0c7ed4915561efb52
unknown <2214290039@ACADEMICO.IESB.BR>
2024-03-15 21:32:04 -0300

    Pratica 05

--- aulas/aula05/index.js
+++ aulas/aula05/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const routerProdutos = require("./router")
+const routerProdutos = require("./router");
 const app = express();
+
+app.use(express.json());
 
 app.use(routerProdutos);
 
-app.listen(3000, () => {
-    console.log("Api esta on!")
+app.listen(3000, function(){
+    console.log("API está ON!");
 })
 
 module.exports = app;
